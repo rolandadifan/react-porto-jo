df7b8b2d2d7e38e9292e257571971c4ded2fe93c
rolandadifandana <rolanda.difandana@adira.co.id>
2021-08-21 16:50:51 +0700

    update cv

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,36 +1,40 @@
 import React from 'react'
 import Typed from 'react-typed';
 import Slide from 'react-reveal/Slide';
+import CV from "../asset/document/CV_Jofardho Adlinnas.pdf";
 
 
 const Header = () => {
     return (
-        <div>
+      <div>
         <div className="header-wrapper">
-            <div className="main-info">
-                <Slide bottom>
-                    <h1>Hi, Saya Jofardho Adlinnas </h1>
-                </Slide>
-                <Typed className="typed-text"
-                 strings={["UI DESIGN", "UX DESIGN","WEB DESIGN"]}
-                 typeSpeed={70}
-                 backSpeed={60}
-                 loop
-                 />
-                 <div className="row button-head-wrap mt-5">
-                     <div className="col">
-                         <a href="#contact" className="btn-main-offer">CONTACT</a>
-                     </div>
-                     <div className="col">
-                         <a href="/" className="btn-main-offer">Unduh CV</a>
-                     </div>
-                 </div>
-                 
+          <div className="main-info">
+            <Slide bottom>
+              <h1>Hi, Saya Jofardho Adlinnas </h1>
+            </Slide>
+            <Typed
+              className="typed-text"
+              strings={["UI DESIGN", "UX DESIGN", "WEB DESIGN"]}
+              typeSpeed={70}
+              backSpeed={60}
+              loop
+            />
+            <div className="row button-head-wrap mt-5">
+              <div className="col">
+                <a href="#contact" className="btn-main-offer">
+                  CONTACT
+                </a>
+              </div>
+              <div className="col">
+                <a href={CV} download className="btn-main-offer">
+                  Unduh CV
+                </a>
+              </div>
             </div>
-            
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    );
 }
 
 export default Header
